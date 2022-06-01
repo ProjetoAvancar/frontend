d2bd8a3796c3e70cd884f7669089be06d35d54c6
Daniel <danielmarcionilo2002@gmail.com>
2022-06-01 00:16:39 -0300

    Adicionado component para apresentação dos criadores do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DevsComponent } from './devs/devs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevsComponent
   ],
   imports: [
     BrowserModule,
