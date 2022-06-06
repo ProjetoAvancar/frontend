9f285ad7190d3375bd396cda71603b7284855d78
Daniel <danielmarcionilo2002@gmail.com>
2022-06-06 15:15:46 -0300

    Componente cards da home page adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
