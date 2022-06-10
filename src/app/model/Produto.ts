f066357f3669c67484e61dfc2422c7b770803ae6
Gabacles <g.dalrovere@gmail.com>
2022-06-10 13:17:50 -0300

    Implementação da model produto e de seu service

---
+++ src/app/model/Produto.ts
@@ -0,0 +1,9 @@
+import { User } from './User';
+export class Produto {
+  public id: number
+  public nome: string
+  public valor: number
+  public foto: string
+  //public categoria: Categoria
+  public usuario: User
+}
