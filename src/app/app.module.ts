4feab95a0903699398fc80de91d8addf8f0f9a79
Marcos Sel <marcosrsel@gmail.com>
2022-06-13 11:51:50 -0300

    layout editar usuario versão 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    EditarUsuarioComponent
   ],
   imports: [
     BrowserModule,
